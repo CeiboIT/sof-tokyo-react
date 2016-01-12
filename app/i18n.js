c0f909f68ff84b1930bb6b2ce6ef3b83d86f58ce
epotignano <epotignano@gmail.com>
2016-01-12 01:41:37 -0300

    #closes 18

--- app/i18n.js
+++ app/i18n.js
@@ -3,25 +3,34 @@
  */
 
 var I18n = require('react-native-i18n');
+I18n.locale = 'ja-JP';
+
+
 class I18nService {
     constructor(){
         this.I18n = I18n;
+        // intialization
         this.I18n.translations = I18n.translations;
+
+        //Set the default language of the app if there are no
+        // locales for charge!
+        this.I18n.missingTranslation = (arg) => {
+            return this.I18n.translations['ja-JP'][arg]
+        };
+
     }
 
     set(langKey, translationsObj) {
-
         if(!this.I18n.translations[langKey])  this.I18n.translations[langKey] = {};
-
         var _keys = Object.keys(translationsObj);
         _keys.map((key) => {
             this.I18n.translations[langKey][key] = translationsObj[key]
         });
-
-        console.warn(JSON.stringify(I18n.translations));
     };
 
     getTranslations () {
+
+
         return this.I18n
     };
 }
