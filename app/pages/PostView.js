5b1b3d25eb0893201b8607e56d319766a40751ef
epotignano <epotignano@gmail.com>
2016-01-19 21:37:49 -0300

    Avances en render Post

--- app/pages/PostView.js
+++ app/pages/PostView.js
@@ -7,13 +7,23 @@ var Dimensions = require('Dimensions');
 var windowSize = Dimensions.get("window");
 var ResponsiveImage = require('react-native-responsive-image');
 var PostStream = require("../services/Streams").getStream("Post");
+var Storage = require("../services/Storage").getInstance();
 var api = require("../utils/api/PostApi");
+var GiftedSpinner = require('react-native-gifted-spinner');
+
+var Icon = require("react-native-vector-icons/FontAwesome"),
+    screen = Dimensions.get('window');
 
 var {
-    View,
-    Text,
+    PixelRatio,
+    ScrollView,
     StyleSheet,
-    TouchableHighlight
+    Text,
+    TouchableOpacity,
+    TouchableHighlight,
+    View,
+    ListView,
+    Dimensions,
     } = React;
 
 var styles = StyleSheet.create({
@@ -35,11 +45,9 @@ var styles = StyleSheet.create({
         width: windowSize.width * 0.4,
         marginVertical:5
     },
-
     navIcon: {
 
     }
-
 });
 
 
@@ -48,43 +56,157 @@ var imageSizes ={
     height: windowSize.height * 0.6
 };
 
+var PostId;
 
-class PostView extends React.Component {
-    constructor(props) {
-        super(props);
-        api.RetrievePost(props.id);
+var PostView = React.createClass({
 
-        this.state = {
-            postData: {}
-        };
+    getInitialState() {
+        return {
+            post: {},
+            isLoading:true
+        }
+    },
 
-        PostStream.subscribe((response) => {
-            this.setState({
-                postData: response['post']
-            });
+    _renderCommentsList: function() {
+        return <View style={styles.sectionSpacing}>
+            <View style={styles.separator} />
+            <Text style={styles.heading}>Comments</Text>
+            <View style={styles.separator} />
+            <ListView
+                ref="commentsView"
+                dataSource={this.state.dataSource}
+                renderRow={this.renderRow}
+                automaticallyAdjustContentInsets={false}
+            />
+        </View>
+    },
+
+    componentDidMount(){
+        Storage.load({
+            key:'UserId'
+        }).then(ret => {
+            api.RetrievePost(PostId, ret.data)
         })
-    }
+
+        PostStream.subscribe((data => {
+            this.setState({
+                isLoading: false,
+                postData: data['post']
+            })
+        }))
+    },
 
     render() {
-        return(
+
+        PostId = this.props.id;
+        var _postView = (
             <View style={styles.container}>
-                <View>
-                    <ResponsiveImage source={{uri: this.state.postData.thumbnail}}
-                                     initWidth={imageSizes.width} initHeight={imageSizes.height}
-                    />
-                </View>
-                <View>
-                    <Text style={styles.title}> { this.state.postData.title}</Text>
-                </View>
-                <View>
-                </View>
-            </View>
-        )
+                <Text>{JSON.stringify(this.state.postData)}</Text>
+            </View>)
+        var _render = (!this.state.isLoading) ? _postView :  (<GiftedSpinner/>) ;
+        return _render;
     }
-}
+})
+
+
+var styles = StyleSheet.create({
+    spinner: {
+        marginTop: 20,
+        width: 50
+    },
+    a: {
+        fontWeight: "300",
+        color: "#ea4c89"
+    },
+    p: {
+        marginBottom: 0,
+        flexDirection: "row",
+        marginTop: 0,
+    },
+    invisibleView: {
+        flex: 1,
+        position: "absolute",
+        top: 0,
+        left: 0,
+        bottom: 0,
+        right:0
+    },
+    customModalImage: {
+        height: screen.height / 2
+    },
+    headerContent: {
+        flex: 1,
+        paddingBottom: 20,
+        paddingTop: 40,
+        alignItems: "center",
+        width: screen.width,
+        backgroundColor: "#fff"
+    },
+    shotTitle: {
+        fontSize: 16,
+        fontWeight: "400",
+        color: "#ea4c89",
+        lineHeight: 18
+    },
+    playerContent: {
+        fontSize: 12
+    },
+    player: {
+        fontWeight: "900",
+        lineHeight: 18
+    },
+    playerAvatar: {
+        borderRadius: 40,
+        width: 80,
+        height: 80,
+        position: "absolute",
+        bottom: 60,
+        left: screen.width / 2 - 40,
+        borderWidth: 2,
+        borderColor: "#fff"
+    },
+    rightPane: {
+        flex: 1,
+        flexDirection: "column",
+        alignItems: "center"
+    },
+    shotDetailsRow: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "white",
+        flexDirection: "row"
+    },
+    shotCounter: {
+        flex: 2,
+        alignItems: "center",
+        justifyContent: "space-between"
+    },
+    shotCounterText: {
+        color: "#333"
+    },
+    mainSection: {
+        flex: 1,
+        alignItems: "stretch",
+        padding: 10,
+        backgroundColor: "white"
+    },
+    separator: {
+        backgroundColor: "rgba(0, 0, 0, 0.1)",
+        height: 1 / PixelRatio.get(),
+        marginVertical: 10,
+    },
+    sectionSpacing: {
+        marginTop: 20
+    },
+    heading: {
+        fontWeight: "700",
+        fontSize: 16
+    }
+});
 
 PostView.PropTypes= {
-    postId: React.PropTypes.object
+    id: React.PropTypes.object
 };
 
 module.exports = PostView;
