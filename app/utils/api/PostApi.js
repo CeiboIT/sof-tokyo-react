d474319bb8e316bc970dac12dde0730cb15b228b
epotignano <epotignano@gmail.com>
2016-01-23 15:17:47 -0300

    PostView api

--- app/utils/api/PostApi.js
+++ app/utils/api/PostApi.js
@@ -5,6 +5,7 @@ var apiConsts  = require("../../constants/api").apiConsts;
 var PostStream = require("../../services/Streams").getStream("Post");
 var productsEndpoint = apiConsts.apiEndpoint + 'products/';
 
+
 var api = {
     async RetrievePost(postId, userId) {
         try {
@@ -29,6 +30,27 @@ var api = {
             })
             subject.onNext(JSON.parse(actionResult._bodyInit))
         } catch(error){
+            console.warn(error);
+        }
+    },
+
+    async sendComment(comment, postId, subject) {
+        try {
+            let cookie = await storage.load({key: 'cookies'});
+            let commentPost = await fetch(productsEndpoint + 'comments/create',{
+                method: 'POST',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    cookie: cookie.data,
+                    productId: postId,
+                    content: comment
+                })
+            })
+            subject.onNext({type:'comment', data: JSON.parse(commentPost._bodyInit)})
+        }catch(error){
             console.warn(error);
         }
     }
