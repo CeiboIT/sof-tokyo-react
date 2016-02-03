c513620765213f837c997efb6619085128007cd5
sebastian <sebastian.fondra@gmail.com>
2016-02-03 17:59:14 -0300

    api upload url

--- app/constants/api.js
+++ app/constants/api.js
@@ -6,7 +6,7 @@ export const apiConsts = {
     main: 'https://sof-tokyo-node-server.herokuapp.com/',
     apiEndpoint: 'https://sof-tokyo-node-server.herokuapp.com/api/',
     //imageCloudServer: 'https://api.cloudinary.com/v1_1/dervvwuw6/image/',
-    imageCloudServer: 'http://192.168.0.10:3000/',
+    imageCloudServer: 'http://192.168.0.10:9000/api/images/',
     ownerEmail: 'epotignano@gmail.com'
 };
 
